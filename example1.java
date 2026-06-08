f7c2c375cc8f7ccea41c80a49f50b8c0d84fd7e1
AiiySK <g-58214921@moe-dl.edu.my>
2026-06-08 09:18:10 +0800

    Add files via upload
    This is extended file for function Upload File

---
+++ example1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author H P
+ */
+import java.util.Scanner;
+
+public class example1 {
+    
+    
+    
+    public static void main(String []args){
+        
+        Scanner input=new Scanner (System.in);
+        String nama;
+        int tahun;
+        
+        System.out.println("Pelajar : " );
+        nama=input.nextLine();
+        System.out.println("Tahun : " );
+        tahun=input.nextInt();
+        
+    }
+    
+}
+
